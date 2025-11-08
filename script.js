4a2eaa6ea57b41fc8ee0a482c648f0a27a73205e
SolomonChinonso <151050782+SolomonChinonso@users.noreply.github.com>
2025-11-08 16:04:35 +0100

    Refactor script.js for improved functionality and style

--- script.js
+++ script.js
@@ -1,57 +1,84 @@
-// declaring variables for nameInput and text
+let input = document.getElementById("nameInput");
+let greet = document.getElementById("greetings");
 
-let input = document.getElementById("nameInput")
-let greet = document.getElementById("greeting")
 
-// addEventListener to the input and text conditions using if-else loop
+let box = document.getElementById("moodBox")
+let grow = document.getElementById("btnGrow")
+let shrink = document.getElementById("btnShrink")
 
-input.addEventListener("input",()=>{
-   let name = input.value;
-  if(name === ""){
-    greeting.innerText = "Hello, friend!"
-  } else{
-    greet.innerText = "Hello!" +"-" +input.value;
-    greeting.style.color = "green";
-    greeting.style.fontWeight = "bold";
-  }
- 
-})
-
-// declaring variables for box and buttons
-
-let box = document.getElementById("box")
-let grow = document.getElementById("grow")
-let shrink = document.getElementById("shrink")
-let button = document.getElementsByClassName("btn")
-let message = document.getElementById("msg")
-let motMessage = document.getElementById("motMsg")
-let body = document.getElementById("body")
-
-// addition of eventListeners and styles to the box and button
+box.style.border = "2px solid black"
+box.style.padding = "50px"
+box.style.borderRadius = "10px"
+box.style.display = "flex"
+box.style.flexDirection = "column"
+box.style.backgroundColor = "gray"
 
 grow.addEventListener("click",()=>{
-  box.style.backgroundColor = "black"
-  box.style.padding = "30px"
+  box.style.width = "150px"
+  box.style.height = "150px"
   grow.style.display = "none"
   shrink.style.display = "block"
-  box.style.width = "100%"
-  message.style.color = "white"
-  message.style.fontSize = "30px"
-  motMessage.style.display = "block"
-  motMessage.style.color = "white"
-  body.style.backgroundColor = "whiteSmoke"
-  greeting.style.color 
-  = "black"
+  shrink.style.position = "relative"
+  shrink.style.bottom = "-100px"
 })
 
 shrink.addEventListener("click",()=>{
-  box.style.backgroundColor = "white"
-  box.style.padding = "5px"
+  box.style.width = "50px"
+  box.style.height = "50px"
   shrink.style.display = "none"
   grow.style.display = "block"
-  box.style.width = "60%"
-  message.style.color = "black"
-  message.style.fontSize = "15px"
- motMessage.style.display = "none"
-//  body.style.backgroundColor = "white"
+  shrink.style.position = "relative"
+  shrink.style.bottom = "-100px"
 })
+
+let day = document.getElementById("light")
+let night = document.getElementById("dark")
+let body = document.getElementById("entireBody")
+
+
+day.addEventListener("click",()=>{
+  body.style.backgroundColor = "white"
+  body.style.color = "black"
+  night.style.display = "block"
+  day.style.display = "none"
+})
+
+night.addEventListener("click",()=>{
+  body.style.backgroundColor = "black"
+  body.style.color = "white"
+  night.style.display = "none"
+  day.style.display = "block"
+})
+
+let show = document.getElementById("btnShow")
+let hide = document.getElementById("btnHide")
+let message = document.getElementById("msg")
+
+message.style.display = "none"
+
+show.addEventListener("click",()=>{
+  message.style.display = "block"
+  show.style.display = "none"
+  hide.style.display = "block"
+})
+
+hide.addEventListener("click",()=>{
+  message.style.display = "none"
+  show.style.display = "block"
+  hide.style.display = "none"
+})
+
+body.style.display = "flex"
+body.style.flexDirection = "column"
+body.style.justifyContent ="center"
+body.style.alignItems = "center"
+
+input.addEventListener("input",()=>{
+let name = input.value;
+  if(name === ""){
+    greet.innerText = "Hello, My G"
+  }else{greetings.innerText = "Hello! " +name}
+  greet.style.fontWeight = "bold"
+  greet.style.color = "green"
+});
+
